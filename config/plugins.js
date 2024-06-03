f615893f51f08d18fead33c091762e3d62b48024
Manuel yemoh <manuelyemoh@gmail.com>
2024-06-03 16:37:30 +0100

    added delete function

--- config/plugins.js
+++ config/plugins.js
@@ -24,6 +24,7 @@ module.exports = ({ env }) => ({
           // Explicitly setting ACL to null
           ACL: null,
         },
+        delete: {}, // Add this line to configure the delete action
       },
     },
   },
